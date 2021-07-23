9a944387f76b03a42c465a69f933659aa0ea7ccd
caisarl76 <caisarl76@gmail.com>
2021-07-23 17:14:27 +0900

    add lt dataset

---
+++ models/losses.py
@@ -0,0 +1,119 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+eps = 1e-7
+
+def focal_loss(input_values, gamma):
+    """Computes the focal loss"""
+    p = torch.exp(-input_values)
+    loss = (1 - p) ** gamma * input_values
+    return loss.mean()
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, cls_num_list=None, weight=None, gamma=0.):
+        super(FocalLoss, self).__init__()
+        assert gamma >= 0
+        self.gamma = gamma
+        self.weight = weight
+
+    def _hook_before_epoch(self, epoch):
+        pass
+
+    def forward(self, output_logits, target):
+        return focal_loss(F.cross_entropy(output_logits, target, reduction='none', weight=self.weight), self.gamma)
+
+
+class CrossEntropyLoss(nn.Module):
+    def __init__(self, cls_num_list=None, reweight_CE=False):
+        super().__init__()
+        if reweight_CE:
+            idx = 1  # condition could be put in order to set idx
+            betas = [0, 0.9999]
+            effective_num = 1.0 - np.power(betas[idx], cls_num_list)
+            per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+            per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)
+            self.per_cls_weights = torch.tensor(per_cls_weights, dtype=torch.float, requires_grad=False)
+        else:
+            self.per_cls_weights = None
+
+    def to(self, device):
+        super().to(device)
+        if self.per_cls_weights is not None:
+            self.per_cls_weights = self.per_cls_weights.to(device)
+
+        return self
+
+    def forward(self, output_logits, target):  # output is logits
+        return F.cross_entropy(output_logits, target, weight=self.per_cls_weights)
+
+
+class LDAMLoss(nn.Module):
+    def __init__(self, cls_num_list=None, max_m=0.5, s=30, reweight_epoch=-1):
+        super().__init__()
+        if cls_num_list is None:
+            # No cls_num_list is provided, then we cannot adjust cross entropy with LDAM.
+            self.m_list = None
+        else:
+            self.reweight_epoch = reweight_epoch
+            m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
+            m_list = m_list * (max_m / np.max(m_list))
+            m_list = torch.tensor(m_list, dtype=torch.float, requires_grad=False)
+            self.m_list = m_list
+            assert s > 0
+            self.s = s
+            if reweight_epoch != -1:
+                # CB loss
+                idx = 1  # condition could be put in order to set idx
+                betas = [0, 0.9999]
+                effective_num = 1.0 - np.power(betas[idx], cls_num_list)
+                per_cls_weights = (1.0 - betas[idx]) / np.array(effective_num)
+                per_cls_weights = per_cls_weights / np.sum(per_cls_weights) * len(cls_num_list)  # * class number
+                # the effect of per_cls_weights / np.sum(per_cls_weights) can be described in the learning rate so the math formulation keeps the same.
+                self.per_cls_weights_enabled = torch.tensor(per_cls_weights, dtype=torch.float, requires_grad=False)
+            else:
+                self.per_cls_weights_enabled = None
+                self.per_cls_weights = None
+
+    def to(self, device):
+        super().to(device)
+        if self.m_list is not None:
+            self.m_list = self.m_list.to(device)
+
+        if self.per_cls_weights_enabled is not None:
+            self.per_cls_weights_enabled = self.per_cls_weights_enabled.to(device)
+
+        return self
+
+    def _hook_before_epoch(self, epoch):
+        if self.reweight_epoch != -1:
+            self.epoch = epoch
+
+            if epoch > self.reweight_epoch:
+                self.per_cls_weights = self.per_cls_weights_enabled
+            else:
+                self.per_cls_weights = None
+
+    def get_final_output(self, output_logits, target):
+        x = output_logits
+
+        index = torch.zeros_like(x, dtype=torch.uint8, device=x.device)
+        index.scatter_(1, target.data.view(-1, 1), 1)  # one-hot index
+
+        index_float = index.float()
+        batch_m = torch.matmul(self.m_list[None, :], index_float.transpose(0, 1))
+
+        batch_m = batch_m.view((-1, 1))
+        x_m = x - batch_m * self.s
+
+        final_output = torch.where(index, x_m, x)
+        return final_output
+
+    def forward(self, output_logits, target):
+        if self.m_list is None:
+            return F.cross_entropy(output_logits, target)
+
+        final_output = self.get_final_output(output_logits, target)
+        return F.cross_entropy(final_output, target, weight=self.per_cls_weights)
